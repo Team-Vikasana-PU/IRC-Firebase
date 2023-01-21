f324967cfa9d0d902ce47828eced3e26044bf751
Umraz Khan <umraz@under25.club>
2023-01-21 14:56:27 +0530

    map update

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,7 @@ const Map = () => {
             <Navbar />
             <div className='w-full flex flex-col items-center pt-32 px-10'>
                 <h2 className='font-circular text-3xl md:text-6xl text-gradientPink mb-10'>University Map</h2>
-                <iframe src="https://www.google.com/maps/d/u/0/embed?mid=10sQu62x6fEeZLRHnhSWn-of1PQ5JI0c&ehbc=2E312F" className='w-full md:w-8/12' height="600"></iframe>
+                <iframe src="https://www.google.com/maps/d/u/0/edit?mid=1GSYIRYlUDzH-L5fcGAArX4vPWffmFO4&usp=sharing" className='w-full md:w-8/12' height="600"></iframe>
             </div>
         
         </>
